4eb1785bc050f4d9619016520b082289a8c80aae
DenislavD <az3@abv.bg>
2016-12-14 19:09:38 +0200

    Moving API to separate project & finalizing it

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,154 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise; //mpromise is deprecated, use native ES6 promises
+var Loc = mongoose.model('Location'); //instantiate the model
+
+var sendJsonResponse = function(res, status, content) {
+    res.status(status);
+    res.json(content);
+};
+
+//http://localhost:3000/api/locations?lng=-0.79925&lat=51.37809&dis=50
+module.exports.locationsListByDistance = function(req, res) {
+    var lng = parseFloat(req.query.lng);
+    var lat = parseFloat(req.query.lat);
+    var dis = parseInt(req.query.dis);
+    var point = {
+        type: "Point",
+        coordinates: [lng, lat]
+    };
+    var geoOptions = {
+        spherical: true,
+        maxDistance: dis * 1000,
+        num: 10,
+    };
+
+    if(!lng || !lat) {
+        sendJsonResponse(res, 404, {"message": "lng and lat query parameters are required"});
+        return;
+    }
+
+    //geoNear gets executed immediately, no need for exec()
+    Loc.geoNear(point, geoOptions, function(err, results, stats) {
+        if(err) {
+            sendJsonResponse(res, 404, err);
+        } else {
+            var locations = [];
+            results.forEach(function(doc) {
+                locations.push({
+                    distance: doc.dis,
+                    name: doc.obj.name,
+                    address: doc.obj.address,
+                    rating: doc.obj.rating,
+                    facilities: doc.obj.facilities,
+                    _id: doc.obj._id,
+                });
+            });
+            sendJsonResponse(res, 200, locations);
+        }
+
+    });
+};
+
+module.exports.locationsCreate = function(req, res) {
+    var facilities = req.body.facilities && req.body.facilities.split(',') || [];
+    Loc.create({
+        name: req.body.name,
+        address: req.body.address,
+        facilities: facilities,
+        coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+        openingTimes: [{
+                days: req.body.days1,
+                opening: req.body.opening1,
+                closing: req.body.closing1,
+                closed: req.body.closed1,
+            },
+            {
+                days: req.body.days2,
+                opening: req.body.opening2,
+                closing: req.body.closing2,
+                closed: req.body.closed2,
+            }],
+    }, function(err, location) {
+        if(err) {
+            sendJsonResponse(res, 400, err);
+        } else {
+            sendJsonResponse(res, 201, location);
+        }
+    });
+};
+
+module.exports.locationsReadOne = function(req, res) {
+    if(req.params && req.params.locationid) {
+        Loc.findById(req.params.locationid).exec(function(err, location) {
+            if(err) {
+                sendJsonResponse(res, 404, err);
+                return;
+            } else if(!location) {
+                sendJsonResponse(res, 404, {"message": "locationid not found"});
+                return;
+            }
+            sendJsonResponse(res, 200, location);
+        });
+    } else {
+        sendJsonResponse(res, 404, {"message": "No locationid in request"});
+    }
+};
+
+module.exports.locationsUpdateOne = function(req, res) {
+    if (!req.params.locationid) {
+        sendJsonResponse(res, 404, {"message": "Not found, locationid is required"});
+        return;
+    }
+
+    Loc.findById(req.params.locationid).select('-reviews -rating')
+        .exec(function(err, location) {
+            if (!location) {
+                sendJsonResponse(res, 404, {"message": "locationid not found"});
+                return;
+            } else if (err) {
+                sendJsonResponse(res, 400, err);
+                return;
+            }
+
+            location.name = req.body.name;
+            location.address = req.body.address;
+            location.facilities = req.body.facilities.split(",");
+            location.coords = [parseFloat(req.body.lng), parseFloat(req.body.lat)];
+            location.openingTimes = [{
+                days: req.body.days1,
+                opening: req.body.opening1,
+                closing: req.body.closing1,
+                closed: req.body.closed1,
+            }, {
+                days: req.body.days2,
+                opening: req.body.opening2,
+                closing: req.body.closing2,
+                closed: req.body.closed2,
+            }];
+            location.save(function(err, location) {
+                if (err) {
+                    sendJsonResponse(res, 404, err);
+                } else {
+                    sendJsonResponse(res, 200, location);
+                }
+            });
+        }
+    );
+};
+
+module.exports.locationsDeleteOne = function(req, res) {
+    var locationid = req.params.locationid;
+    if (locationid) {
+        Loc.findByIdAndRemove(locationid).exec(function(err, location) {
+            if (err) {
+                console.log(err);
+                sendJsonResponse(res, 404, err);
+                return;
+            }
+            console.log("Location id " + locationid + " deleted");
+            sendJsonResponse(res, 204, null);
+        });
+    } else {
+        sendJsonResponse(res, 404, {"message": "No locationid"});
+    }
+};
---
+++ app_api/models/locations.js
@@ -0,0 +1,92 @@
+var mongoose = require('mongoose');
+
+var openingTimeSchema = new mongoose.Schema({
+    days: {
+        type: String,
+        required: true,
+    },
+    opening: String,
+    closing: String,
+    closed: {
+        type: Boolean,
+        required: true,
+    }
+});
+
+var reviewSchema = new mongoose.Schema({
+    author: String,
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5
+    },
+    reviewText: String,
+    createdOn: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+var locationSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    address: String,
+    rating: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 5,
+    },
+    facilities: [String],
+    coords: {
+        type: [Number],
+        index: '2dsphere',
+    },
+    openingTimes: [openingTimeSchema],
+    reviews: [reviewSchema]
+});
+
+//define the model (need to be done here because we defined the schema here)
+mongoose.model('Location', locationSchema);
+
+//populate data
+/*
+var loc = new Location({
+    name: 'Cafe Hero',
+    address: '51 Hero Street, Reading, RGA 2PS',
+    rating: 2,
+    facilities: ['Hot drinks', 'Premium wifi'],
+    coords: [-0.9190884, 51.955041],
+    openingTimes: [{
+        days: 'Monday - Wednesday',
+        opening: '8:00am',
+        closing: '8:00pm',
+        closed: false
+    }, {
+        days: 'Thurdsay - Saturday',
+        closed: true
+    }, {
+        days: 'Sunday',
+        closed: true
+    }],
+    reviews: [{
+        author: 'Simon Holmes',
+        rating: 1,
+        timestamp: '29 July 2013',
+        reviewText: 'What a great place. I can\'t say enough good things about it.'
+    }, {
+        author: 'Charlie Chaplin',
+        rating: 3,
+        timestamp: '29 June 2013',
+        reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+    }]
+});
+loc.save(function (err) {
+    if (err) {
+        console.log(err);
+    }
+    // saved!
+});*/
